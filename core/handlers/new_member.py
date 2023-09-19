e7b644c7d7855cd566ad3c85882eccf3e2f6ab76
akchonya <akchonyahokagivna@gmail.com>
2023-09-19 14:00:24 +0300

    removed unnecessary bot: Bot

--- core/handlers/new_member.py
+++ core/handlers/new_member.py
@@ -1,4 +1,4 @@
-from aiogram import F, Router, Bot
+from aiogram import F, Router
 from aiogram.filters import IS_MEMBER, IS_NOT_MEMBER, ChatMemberUpdatedFilter
 from aiogram.types import ChatMemberUpdated
 
@@ -6,7 +6,7 @@ from aiogram.types import ChatMemberUpdated
 new_member_router = Router()
 
 @new_member_router.chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
-async def new_member(event: ChatMemberUpdated, bot: Bot):
+async def new_member(event: ChatMemberUpdated):
         
         await event.answer(f"<b>привіт, {event.new_chat_member.user.first_name}!</b>" +
                     "\n\nможеш подивитися список доступних команд " +
