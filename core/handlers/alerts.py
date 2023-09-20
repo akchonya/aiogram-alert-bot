9e634472c01f5a32907e2f23334dc5bc31daf9df
akchonya <akchonyahokagivna@gmail.com>
2023-09-20 19:38:39 +0300

    formatted date

--- core/handlers/alerts.py
+++ core/handlers/alerts.py
@@ -43,7 +43,7 @@ async def alerts_handler(message: types.Message, bot: Bot):
                 lviv_status = lviv
 
                 dt_now = datetime.datetime.now()
-                formatted_date = f"{dt_now.year}-{dt_now.month}-{dt_now.day} | {dt_now.hour}:{dt_now.minute}:{dt_now.second}"
+                formatted_date = f"{dt_now.year}-{dt_now.month}-{dt_now.day}|{dt_now.hour}:{dt_now.minute}:{dt_now.second}"
 
                 await message.answer(f"[{formatted_date}] Alert update: {lviv}",
                                     reply_markup=types.ReplyKeyboardRemove())
