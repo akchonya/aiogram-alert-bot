95113a3b807dd883926e08741b5226c99bf6d29d
akchonya <akchonyahokagivna@gmail.com>
2023-09-20 19:12:49 +0300

    fixing kbs

--- core/keyboards/draw_vahta_kb.py
+++ core/keyboards/draw_vahta_kb.py
@@ -21,22 +21,22 @@ resize_keyboard=True
 )
 
 vahta_rows_kb = ReplyKeyboardMarkup(keyboard=[
-    KeyboardButton(text="1"),
+    [KeyboardButton(text="1"),
     KeyboardButton(text="2"),
     KeyboardButton(text="3"),
     KeyboardButton(text="4"),
     KeyboardButton(text="5"),
-    KeyboardButton(text="6")
+    KeyboardButton(text="6")]
 ],
 resize_keyboard=True)
 
 vahta_columns_kb = ReplyKeyboardMarkup(keyboard=[
-    KeyboardButton(text="1"),
+    [KeyboardButton(text="1"),
     KeyboardButton(text="2"),
     KeyboardButton(text="3"),
     KeyboardButton(text="4"),
     KeyboardButton(text="5"),
     KeyboardButton(text="6"),
-    KeyboardButton(text="7")
+    KeyboardButton(text="7")]
 ],
 resize_keyboard=True)
