fdc832de60c39073fa9fd932d3be028796de6222
akchonya <akchonyahokagivna@gmail.com>
2023-09-25 00:33:53 +0300

    file_id

--- core/handlers/alerts.py
+++ core/handlers/alerts.py
@@ -48,7 +48,7 @@ async def alerts_handler(message: types.Message, bot: Bot):
             # If there is an alert - send and pin the video, then send a corresponding message 
             if lviv == "active":
                 msg = await bot.send_video(DORM_CHAT_ID, 
-                                            video="BAACAgIAAxkBAAEmKHBlDCfaQyRViympN1eh5vsqU0oQ8wACNx4AAmqumUr1ey8JH10sPDAE")
+                                            video="BAACAgIAAxkBAAEmP_hlEKsdrCD---dbYU9gM0dm-cUq2wACNx4AAmqumUr1ey8JH10sPDAE")
                 await bot.pin_chat_message(DORM_CHAT_ID, msg.message_id, True)
                 await bot.send_message(DORM_CHAT_ID, "🚨 <b>ТРИВОГА!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</b>\n" +
                                     "ПАКУЙТЕ СМАКОЛИКИ І У СХОВИЩЕ \n\n" +
