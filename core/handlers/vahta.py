5154bf30a2d27ca5580ce97fe5cf98b727c34ea2
akchonya <akchonyahokagivna@gmail.com>
2023-10-16 23:27:19 +0300

    added response to a text q

--- core/handlers/vahta.py
+++ core/handlers/vahta.py
@@ -3,7 +3,7 @@
 '''
 
 
-from aiogram import types, Router
+from aiogram import types, Router, F
 from aiogram.filters import Command
 from aiogram.types import ReplyKeyboardRemove
 
@@ -16,4 +16,10 @@ async def vahta_handler(message: types.Message):
     await message.answer_photo(photo=photo, caption="Прошу! Усе що знаю про графік наших (ваших) вахтерів:",
                                reply_markup=ReplyKeyboardRemove())
 
+@vahta_router.message(F.text.casefold().contains("хто на вахті"))
+async def vahta_text_handler(message: types.Message):
+    photo = types.FSInputFile("pillow_bot/vahta.png")
+    await message.answer_photo(photo=photo, caption="мені здалося, чи ви запитали хто на вахті? 🧐\n" +
+                               "якщо так - тримайте ось графік вахтерів на сьогодні.\n\n" +
+                               "а на майбутнє - користуйтеся /vahta")
 
