22abe82bd864267702f4e45fecefb3d5f49ee9dd
akchonya <akchonyahokagivna@gmail.com>
2023-11-01 22:32:05 +0200

    fix html tag bug

--- core/handlers/new_member.py
+++ core/handlers/new_member.py
@@ -2,8 +2,7 @@
 Setting an automatic send of a welcome message for each new user 
 '''
 
-
-from aiogram import F, Router
+from aiogram import F, Router, html
 from aiogram.filters import IS_MEMBER, IS_NOT_MEMBER, ChatMemberUpdatedFilter
 from aiogram.types import ChatMemberUpdated
 
@@ -13,7 +12,7 @@ new_member_router = Router()
 @new_member_router.chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
 async def new_member(event: ChatMemberUpdated):
         
-        await event.answer(f"<b>привіт, {event.new_chat_member.user.first_name}!</b>" +
+        await event.answer(f"<b>привіт, {html.unparse(event.new_chat_member.user.first_name)}!</b>" +
                     "\n\nможеш подивитися список доступних команд " +
                     "в полі набору повідомлення. рекомендую /faq " +
                     "до прочитання.",
