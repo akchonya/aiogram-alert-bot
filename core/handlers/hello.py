0071dee37334624405ed68281367b7eb06c6cfaa
akchonya <akchonyahokagivna@gmail.com>
2023-11-20 15:44:33 +0200

    minor change

--- core/handlers/hello.py
+++ core/handlers/hello.py
@@ -4,21 +4,21 @@ from aiogram.types import ReplyKeyboardRemove, Message
 router = Router()
 
 
-@router.message(F.contains("ні"))
+@router.message(F.text.contains("ні"))
 async def hi_lower_handler(message: Message):
     await message.reply("неllo", reply_markup=ReplyKeyboardRemove())
 
 
-@router.message(F.contains("НІ"))
+@router.message(F.text.contains("НІ"))
 async def hi_upper_handler(message: Message):
     await message.reply("НЕLLO", reply_markup=ReplyKeyboardRemove())
 
 
-@router.message(F.contains("Ні"))
+@router.message(F.text.contains("Ні"))
 async def hi_cap_handler(message: Message):
     await message.reply("Нello", reply_markup=ReplyKeyboardRemove())
 
 
-@router.message(F.contains("нІ"))
+@router.message(F.text.contains("нІ"))
 async def hi_crazy_handler(message: Message):
     await message.reply("нEllO", reply_markup=ReplyKeyboardRemove())
