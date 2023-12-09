9487b5776917ef2d6d2becc849956c07c14d1cb3
akchonya <akchonyahokagivna@gmail.com>
2023-12-09 15:57:53 +0200

    update help_router

--- core/handlers/start.py
+++ core/handlers/start.py
@@ -21,7 +21,7 @@ async def start_handler(message: types.Message):
         f"<b>привіт, {message.from_user.first_name}!</b>\n"
         + "я - ботік помічник для третього гурту лну.\n"
         + "можеш подивитися усі доступні команди в меню.\n\n"
-        + "якщо маєш питання чи пропозиції - звертайся до @FleshkaXDude",
+        + f"якщо маєш питання чи пропозиції - <a href='tg://user?id={ADMIN_IDS[0]}'>звертайся</a>",
         reply_markup=ReplyKeyboardRemove(),
     )
 
@@ -34,6 +34,7 @@ async def help_handler(message: types.Message):
         + "• /faq присилає корисну статтю\n"
         + "• /vahta - графік вахтерів\n"
         + "• /bunt i /rusoriz кидають відповідні стікери\n"
+        + "• /weather_now i /weather_today показують погоду зараз і на сьогодні відповідно"
         + "• за допомогою /donate можеш підтримати розробницю бота\n\n"
         + f"якщо маєш питання чи пропозиції - <a href='tg://user?id={ADMIN_IDS[0]}'>звертайся</a>",
         parse_mode="HTML",
