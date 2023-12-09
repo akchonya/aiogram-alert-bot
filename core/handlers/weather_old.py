10a16452a1840f00ad079dc0d3b4086b344466be
akchonya <akchonyahokagivna@gmail.com>
2023-12-09 15:10:43 +0200

    change weather module

---
+++ core/handlers/weather_old.py
@@ -0,0 +1,65 @@
+import logging
+from datetime import datetime, timedelta
+
+import python_weather
+from aiogram import F, Router, html
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiohttp.client_exceptions import ClientConnectorError
+from python_weather.enums import Locale
+
+
+async def getweather():
+    # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
+    async with python_weather.Client(
+        unit=python_weather.METRIC, locale=Locale.UKRAINIAN
+    ) as client:
+        # fetch a weather forecast from a city
+        return await client.get("Lviv")
+
+
+router = Router()
+
+
+@router.message(Command("weather_now"))
+async def weather_now_handler(message: Message):
+    weather = await getweather()
+
+    msg = (
+        f"{html.bold('погода зараз')}:\n"
+        f"🌡 {weather.current.temperature}°C (відчувається як {weather.current.feels_like}°C)\n"
+        f"📝 {weather.current.description.lower()}\n\n"
+    )
+
+    await message.answer(msg)
+
+
+@router.message(Command("weather_today"))
+async def weather_today_handler(message: Message):
+    # Catch the error
+    try:
+        weather = await getweather()
+    except ClientConnectorError as e:
+        logging.error(f"Error fetching weather: {e}")
+        await message.answer("⚠️ сервер з погодою не відповідає, спробуйте пізніше")
+
+    initial_msg = f"{html.bold('прогноз на сьогодні:')}\n"
+    msg = f"{html.bold('прогноз на сьогодні:')}\n"
+    forecast = [i for i in weather.forecasts][0]
+
+    for i, hourly in enumerate(forecast.hourly):
+        if (datetime.now() + timedelta(hours=1)).time() < hourly.time:
+            msg += "🔸🔹"[i % 2]
+            emoji = html.code("\U0001faac")
+            msg += f" {html.bold('{:02d}:{:02d}'.format(hourly.time.hour, hourly.time.minute))}: {hourly.temperature}°C ({emoji} {hourly.feels_like}°C), {hourly.description.lower()}\n"
+
+    print(initial_msg + "\n" + msg)
+    if msg == initial_msg:
+        msg += "🙄 нема вже шо прогрозувати, ви час бачили? до завтра!!"
+
+    await message.answer(msg)
+
+
+@router.message(F.text.casefold().contains("погод"))
+async def weather_text_handler(message: Message):
+    await weather_now_handler(message)
---
+++ core/utils/async_weather.py
@@ -0,0 +1,113 @@
+import datetime
+import logging
+import time
+
+import aiohttp
+from aiogram import html
+
+
+# Define global text variables
+ERROR_MESSAGE = f"⚠️ {html.bold('щось не так з сервером')}\nприйдеться виходити на балкон дивитися погоду"
+FEELS_LIKE_EMOJI = html.code("\U0001faac")
+
+
+# A funcion to get a response from API
+async def make_async_request(url):
+    # Get the start time
+    start_time = time.time()
+
+    async with aiohttp.ClientSession() as session:
+        # Try getting the URL
+        try:
+            async with session.get(url) as response:
+                # Get the end time
+                end_time = time.time()
+
+                # Print the execution time
+                execution_time = end_time - start_time
+                logging.info(f"Execution time: {execution_time} seconds")
+
+                # Check the response status
+                # If it's 200 -> return json
+                if response.status == 200:
+                    response_json = await response.json()
+                    return response_json
+
+                # Else return None
+                else:
+                    logging.error(f"Received non-200 response: {response.status}")
+                    return None
+
+        # If there is an exception -> log it and return None
+        except Exception as e:
+            logging.error(f"An error occurred during the request: {e}")
+            return None
+
+
+async def weather_now(
+    API_KEY,
+    lat=49.8252088,
+    lon=24.0780526,  # Lviv LNU Dorm 3 coordinates
+):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&lang=uk&units=metric"  # OWM endpoint
+    response = await make_async_request(url)
+
+    # If response is not None - proceed
+    if not response:
+        logging.error("Something is wrong with the weather_now response.")
+        return ERROR_MESSAGE
+
+    weather_text = (
+        f"{html.bold('погода зараз:')}\n"
+        f"🌡 {round(response['main']['temp'])}°C ({FEELS_LIKE_EMOJI} {round(response['main']['feels_like'])}°C)"
+    )
+
+    return weather_text
+
+
+# A weather forecast for the day
+async def weather_forecast(
+    API_KEY,
+    lat=49.8252088,
+    lon=24.0780526,  # Lviv LNU Dorm 3 coordinates
+):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}&lang=uk&units=metric"  # OWM endpoint
+    response = await make_async_request(url)
+
+    # Catching response errors
+    if not response:
+        logging.error("Something is wrong with the weather_now response.")
+        return ERROR_MESSAGE
+
+    # Get the data for only today
+    today_list = response["list"][:8]
+
+    # Initialize a forecast string and get "feels like" emoji
+    forecast_string = html.bold("прогноз на сьогодні:\n")
+
+    # For each forecast
+    for i, forecast in enumerate(today_list):
+        # Get the time from the 'dt_text' property
+        time_str = forecast["dt_txt"][11:16]
+
+        # Convert it to time object
+        dt = datetime.datetime.strptime(time_str, "%H:%M").time()
+
+        # If the time is greater than current -> add to the forecast
+        if dt > datetime.datetime.now().time():
+            # Add an icon
+            forecast_string += "🔸🔹"[i % 2]
+
+            # Round the temperature
+            temp = round(forecast["main"]["temp"])
+
+            # Description
+            description = forecast["weather"][0]["description"]
+
+            # Round the "feels like" temperature
+            feels_like = round(forecast["main"]["feels_like"])
+
+            # Create a forecast entry
+            forecast_string += f"{html.bold(time_str)}: {temp}°C ({FEELS_LIKE_EMOJI} {feels_like}°C), {description}\n"
+
+    return forecast_string
