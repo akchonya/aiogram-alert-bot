1f4ef71f43493b41673bd2851db2fc6f4480613b
akchonya <akchonyahokagivna@gmail.com>
2023-12-11 21:57:18 +0200

    add key words response

--- core/handlers/weather.py
+++ core/handlers/weather.py
@@ -1,6 +1,6 @@
 from aiogram.filters import Command
 from aiogram.types import Message
-from aiogram import Router
+from aiogram import Router, F
 from core.utils.config import OWM_API
 from core.utils.async_weather import weather_now, weather_forecast
 
@@ -18,3 +18,8 @@ async def now(message: Message):
 async def today(message: Message):
     response = await weather_forecast(OWM_API)
     await message.answer(response)
+
+
+@router.message(F.text.casefold().contains("погод"))
+async def weather_text_handler(message: Message):
+    await now(message)
