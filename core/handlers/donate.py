7a48c66839abe749660ef3c70e7cbbcd7e662df4
akchonya <akchonyahokagivna@gmail.com>
2024-01-27 20:57:07 +0200

    add the link

--- core/handlers/donate.py
+++ core/handlers/donate.py
@@ -16,7 +16,7 @@ async def donate_handler(message: types.Message):
     await message.answer(
         "<b>Донати приймаються на карти:</b> "
         + "\nприват: 5168752084032468 \nмоно: 4441111136306531\n\n"
-        + "шукайте себе на дошці пошани потім!",
+        + "шукайте себе на <a href='https://telegra.ph/doshka-poshani-LNU-Dorm-3-10-11'>дошці пошани</a> потім!",
         parse_mode="HTML",
         reply_markup=ReplyKeyboardRemove(),
     )
