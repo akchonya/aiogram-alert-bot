b8f61ffda2b79ca4546d8eccc607e4b60444ec8f
akchonya <akchonyahokagivna@gmail.com>
2024-07-05 20:54:03 +0300

    add error handling

--- core/utils/soup.py
+++ core/utils/soup.py
@@ -3,13 +3,19 @@ from bs4 import BeautifulSoup
 
 
 async def fetch(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            return await response.text()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                return await response.text()
+    except Exception as e:
+        print(f"Error fetching URL: {e}")
+        return None
 
 
 async def parse_page(url):
     html = await fetch(url)
+    if html is None:
+        return "Failed to fetch the page."
     soup = BeautifulSoup(html, "html.parser")
     elements_with_class = soup.find_all("a", class_="_self cvplbd")
     text = ""
